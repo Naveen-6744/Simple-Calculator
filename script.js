c08072780ce86379cd442d0a172d3840a3e0f250
Naveen-6744 <139951684+Naveen-6744@users.noreply.github.com>
2023-09-28 16:03:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+ let currentResult = '';
+
+function appendToResult(value) {
+    currentResult += value;
+    document.getElementById('result').value = currentResult;
+}
+
+function calculateResult() {
+    try {
+        currentResult = eval(currentResult);
+        document.getElementById('result').value = currentResult;
+    } catch (error) {
+        currentResult = '';
+        document.getElementById('result').value = 'Error';
+    }
+}
+
+function clearResult() {
+    currentResult = '';
+    document.getElementById('result').value = '';
+}
